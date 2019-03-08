c5ee130b00c5723688acbbdfa447feb4e1e5af4f
sys444555 <369039285@qq.com>
2019-03-08 18:18:00 +0800

    修改Application乱码

--- src/main/java/com/jc/Application.java
+++ src/main/java/com/jc/Application.java
@@ -1,16 +1,9 @@
 package com.jc;
 
-<<<<<<< HEAD
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-=======
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
->>>>>>> f14518782b323302b1d53949ddebc6805e31f635
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
@@ -18,15 +11,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @date ：Created in 2019/3/4 14:46
  * @description：后台启动类
  * @modified By：
- * @version: $version$
+ * @version: 1.0
  */
-<<<<<<< HEAD
 @SpringBootApplication
 @MapperScan("com.jc.mapper")
-=======
 @SpringBootConfiguration
-@EnableAutoConfiguration
->>>>>>> f14518782b323302b1d53949ddebc6805e31f635
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
