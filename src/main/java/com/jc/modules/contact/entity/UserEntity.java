cdf969230a0ebf0dee7958d708f6d260bdb7a6ba
chenhsinglou <921226448@qq.com>
2019-03-29 18:07:45 +0800

    修改contact.js代码

--- src/main/java/com/jc/modules/contact/entity/UserEntity.java
+++ src/main/java/com/jc/modules/contact/entity/UserEntity.java
@@ -10,9 +10,9 @@ import lombok.Data;
 
 /**
  * @author : ChenHsingLou
+ * @version :
  * @date : 2019/3/18 14:56
  * @description : 联系我们
- * @version :
  */
 
 @TableName("jc_user")
@@ -20,22 +20,22 @@ import lombok.Data;
 @ApiModel("联系我们")
 public class UserEntity extends BasicEntity {
     @TableId(type = IdType.AUTO)
-    @ApiModelProperty(value = "主键",name = "id")
+    @ApiModelProperty(value = "主键", name = "id")
     private Integer id;
 
-    @ApiModelProperty(value = "用户名",name="username")
+    @ApiModelProperty(value = "用户名", name = "username")
     private String username;
 
-    @ApiModelProperty(value = "手机号码",name="phone")
+    @ApiModelProperty(value = "手机号码", name = "phone")
     private String phone;
 
-    @ApiModelProperty(value = "邮箱",name="email")
+    @ApiModelProperty(value = "邮箱", name = "email")
     private String email;
 
-    @ApiModelProperty(value = "职业",name="occupation")
+    @ApiModelProperty(value = "职业", name = "occupation")
     private String occupation;
 
-    @ApiModelProperty(value = "信息",name = "message")
+    @ApiModelProperty(value = "信息", name = "message")
     private String message;
 
 
